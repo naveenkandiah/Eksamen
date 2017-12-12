5fd3e58cfa8e7cc73953cdd72e904b45f49ae591
naveenkandiah <naveenkandiah@gmail.com>
2017-12-12 18:49:21 +0100

    Nye Opdateringer

--- app.py
+++ app.py
@@ -1,1 +1,21 @@
-a
+# Produckt Service
+
+from flask import Flask
+from flask-restful import Resource, Api
+
+app = FLask(__name__)
+api = Api(app)
+
+class Product(Resource):
+    def get(self):
+        return {
+            'Product': ['RedBull'
+                        'Cult'
+                        'RockStar'
+                        'X-Ray']
+        }
+
+api.add_resouce(Product, '/')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=80, debug=true)
