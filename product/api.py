99f071f9be54b5a8b95833a483a7b74d45fc47fd
naveenkandiah <naveenkandiah@gmail.com>
2017-12-14 15:40:30 +0100

    1

--- product/api.py
+++ product/api.py
@@ -18,4 +18,4 @@ class Product(Resource):
 api.add_resource(Product, '/')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80, debug='true')
+    app.run(host='0.0.0.0', port=80, debug=True)
